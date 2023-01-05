2c3e52d27d9c0741578ce615c9ec26a9a1249eb2
httpsdwvi <davi.santos123@etec.sp.gov.br>
2023-01-05 17:59:30 -0300

    Tipagem dos pokemons foi adicionada

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,9 +7,24 @@ const Pokemon = (props) => {
     return (
         <div className="card-pokemon">
             <div className="container-img">
-                <img src={pokemon.sprites.front_default}/>
+                <img className="img-pokemon" src={pokemon.sprites.front_default}/>
             </div>
-            {pokemon.name}
+            <div className="card">
+                <div className="card-header">
+                    <h4>{pokemon.name}</h4>
+                    <div>{pokemon.id}</div> 
+                </div>
+                <div className="card-bottom">
+                    <div className="poke-type">
+                        {pokemon.types.map((type,index) => {
+                            return (
+                                <div className="poke-type-txt" key={index}>{type.type.name}</div>
+                            )
+                        }
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
